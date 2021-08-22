6e3873878927513164d927507fb5cc6b90ec6e81
Siddhant <me@madebysid.com>
2021-08-22 10:59:01 +0200

    improve: preact

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
       test: /\.glsl$/,
       use: ["webpack-glsl-loader"],
     });
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom": "preact/compat",
+      });
+    }
 
     return config;
   },
