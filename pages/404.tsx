964d8df6b694f20a0f7b724ad1b18a9d24027d61
Siddhant <me@madebysid.com>
2021-08-23 09:06:26 +0200

    improve: 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,15 +6,24 @@ export default function NotFound() {
   return (
     <>
       <Header />
-      <main className="flex flex-col items-center justify-center mt-64 relative">
-        <h1 className="font-ibm text-9xl">404</h1>
+      <main className="relative flex flex-col items-center justify-center mt-64">
+        <h1 className="z-30 font-ibm text-9xl select-none pointer-events-none">
+          404
+        </h1>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src="/kitty.svg"
           alt="Decorative kitty"
-          className="absolute"
-          style={{ zIndex: -1, top: "-0.75rem" }}
+          className="z-10 absolute cursor-pointer transition translate-y-0 hover:translate-y-6 pt-8"
+          style={{ top: "-2.75rem" }}
         />
+        <div
+          className="z-20 absolute bg-white dark:bg-gray-800 w-[3rem] h-[3rem] rounded-full"
+          style={{
+            borderRadius: "1.2rem",
+            top: "1.4rem",
+          }}
+        ></div>
         <h3 className="text-xl mt-4">There&apos;s nothing here</h3>
         <Link href="/">
           <a title="Back to home" className="mt-32">
