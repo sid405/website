3f5fb3b49c7d917247ffc70d0e0325488a445909
Siddhant <me@madebysid.com>
2021-08-23 08:39:32 +0200

    fix: build

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { Header } from "../components/Header";
 import { Icon } from "../components/Icon";
@@ -10,9 +9,13 @@ export default function NotFound() {
       <Header />
       <main className="flex flex-col items-center justify-center mt-64 relative">
         <h1 className="font-ibm text-9xl">404</h1>
-        <div className="absolute" style={{ zIndex: -1, top: "-0.75rem" }}>
-          <Image src={kitty} alt="Decorative kitty" />
-        </div>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={kitty}
+          alt="Decorative kitty"
+          className="absolute"
+          style={{ zIndex: -1, top: "-0.75rem" }}
+        />
         <h3 className="text-xl mt-4">There&apos;s nothing here</h3>
         <Link href="/">
           <a title="Back to home" className="mt-32">
