399859354fa2e0df2da387e7cd1e7fa9231e8795
Siddhant <me@madebysid.com>
2021-08-23 06:43:41 +0200

    fix: lint

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
 import { Header } from "../components/Header";
 import { Icon } from "../components/Icon";
+import kitty from "../public/kitty.svg";
 
 export default function NotFound() {
   return (
@@ -7,16 +10,15 @@ export default function NotFound() {
       <Header />
       <main className="flex flex-col items-center justify-center mt-64 relative">
         <h1 className="font-ibm text-9xl">404</h1>
-        <img
-          src="/kitty.svg"
-          title="kitty"
-          className="absolute"
-          style={{ zIndex: -1, top: "-0.75rem" }}
-        />
-        <h3 className="text-xl mt-4">There's nothing here</h3>
-        <a title="Back to home" href="/" className="mt-32">
-          <Icon name="arrow-left" />
-        </a>
+        <div className="absolute" style={{ zIndex: -1, top: "-0.75rem" }}>
+          <Image src={kitty} alt="Decorative kitty" />
+        </div>
+        <h3 className="text-xl mt-4">There&apos;s nothing here</h3>
+        <Link href="/">
+          <a title="Back to home" className="mt-32">
+            <Icon name="arrow-left" />
+          </a>
+        </Link>
       </main>
     </>
   );
